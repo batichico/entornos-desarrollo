92429403e3a659f93e2c07e2d8428768f7a8f612
Egoitz <Egoitz@172.30.1.73>
2015-03-17 12:19:21 +0100

    a

--- GitHub/src/org/cuatrovientos/github/Main.java
+++ GitHub/src/org/cuatrovientos/github/Main.java
@@ -7,14 +7,17 @@ package org.cuatrovientos.github;
  * @author Egoitz
  *
  */
-public class GitHub {
+public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stubasds
+      /**
+       * xcvxc
+       */
+		int num;
 	}
 
 }
---
+++ GitHub/src/org/cuatrovientos/github/Other.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.cuatrovientos.github;
+
+/**
+ * @author Egoitz
+ *
+ */
+public class Other {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
